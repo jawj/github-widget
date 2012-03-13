8371c0549ec6422575039abd8671c35fcac9e994
George MacKerron <george@mackerron.co.uk>
2012-03-13 12:39:25 +0000

    Initial commit

---
+++ github-widget.coffee
@@ -0,0 +1,45 @@
+
+# usage: <div class='github-widget' data-user='jawj'></div>
+#        <script src='github-widget.js'></script><link href='github-widget.css' rel='stylesheet' />
+
+# minify: java -jar ~/bin/closure-compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS \
+#         --js github-widget.js --js_output_file github-widget.min.js
+
+go = ->
+  head = document.getElementsByTagName('head')[0]
+  for div, i in document.getElementsByTagName 'div'
+    continue unless div.className is 'github-widget'
+    user = div.getAttribute 'data-user'
+    callback = "githubWidgetJSONPCallback_#{i}"
+    window[callback] = ((div, user) -> 
+      (data) ->
+        tag className: 'gw-clearer', prevSibling: div
+        siteRepoName = "#{user}.github.com"
+        for repo in data.repositories.sort((a, b) -> b.watchers - a.watchers)
+          continue if repo.fork or repo.name is siteRepoName
+          repoOuterTag = tag className: 'gw-repo-outer', parent: div
+          repoTag = tag className: 'gw-repo', parent: repoOuterTag
+          titleTag = tag className: 'gw-title', parent: repoTag
+          statsTag = tag name: 'ul', className: 'gw-stats', parent: titleTag
+          tag name: 'a', href: repo.url, text: repo.name, className: 'gw-name', parent: titleTag
+          tag name: 'li', text: "#{repo.watchers}", className: 'gw-watchers', parent: statsTag
+          tag name: 'li', text: "#{repo.forks}", className: 'gw-forks', parent: statsTag
+          tag className: 'gw-lang', text: repo.language, parent: repoTag
+          tag text: repo.description, className: 'gw-repo-desc', parent: repoTag
+    )(div, user)
+    url  = "http://github.com/api/v2/json/repos/show/#{user}?callback=#{callback}"
+    tag name: 'script', src: url, parent: head
+
+tag = (opts) ->
+  t = document.createElement opts.name ? 'div'
+  for own k, v of opts
+    switch k
+      when 'name' then continue
+      when 'parent' then v.appendChild t
+      when 'prevSibling' then v.parentNode.insertBefore t, v.nextSibling
+      when 'text' then t.appendChild document.createTextNode(v)
+      else t[k] = v
+  t
+
+if document.addEventListener then document.addEventListener 'DOMContentLoaded', go, no
+else if window.attachEvent then window.attachEvent 'onload', go
---
+++ github-widget.js
@@ -0,0 +1,121 @@
+(function() {
+  var go, tag,
+    __hasProp = Object.prototype.hasOwnProperty;
+
+  go = function() {
+    var callback, div, head, i, url, user, _len, _ref, _results;
+    head = document.getElementsByTagName('head')[0];
+    _ref = document.getElementsByTagName('div');
+    _results = [];
+    for (i = 0, _len = _ref.length; i < _len; i++) {
+      div = _ref[i];
+      if (div.className !== 'github-widget') continue;
+      user = div.getAttribute('data-user');
+      callback = "githubWidgetJSONPCallback_" + i;
+      window[callback] = (function(div, user) {
+        return function(data) {
+          var repo, repoOuterTag, repoTag, siteRepoName, statsTag, titleTag, _i, _len2, _ref2, _results2;
+          tag({
+            className: 'gw-clearer',
+            prevSibling: div
+          });
+          siteRepoName = "" + user + ".github.com";
+          _ref2 = data.repositories.sort(function(a, b) {
+            return b.watchers - a.watchers;
+          });
+          _results2 = [];
+          for (_i = 0, _len2 = _ref2.length; _i < _len2; _i++) {
+            repo = _ref2[_i];
+            if (repo.fork || repo.name === siteRepoName) continue;
+            repoOuterTag = tag({
+              className: 'gw-repo-outer',
+              parent: div
+            });
+            repoTag = tag({
+              className: 'gw-repo',
+              parent: repoOuterTag
+            });
+            titleTag = tag({
+              className: 'gw-title',
+              parent: repoTag
+            });
+            statsTag = tag({
+              name: 'ul',
+              className: 'gw-stats',
+              parent: titleTag
+            });
+            tag({
+              name: 'a',
+              href: repo.url,
+              text: repo.name,
+              className: 'gw-name',
+              parent: titleTag
+            });
+            tag({
+              name: 'li',
+              text: "" + repo.watchers,
+              className: 'gw-watchers',
+              parent: statsTag
+            });
+            tag({
+              name: 'li',
+              text: "" + repo.forks,
+              className: 'gw-forks',
+              parent: statsTag
+            });
+            tag({
+              className: 'gw-lang',
+              text: repo.language,
+              parent: repoTag
+            });
+            _results2.push(tag({
+              text: repo.description,
+              className: 'gw-repo-desc',
+              parent: repoTag
+            }));
+          }
+          return _results2;
+        };
+      })(div, user);
+      url = "http://github.com/api/v2/json/repos/show/" + user + "?callback=" + callback;
+      _results.push(tag({
+        name: 'script',
+        src: url,
+        parent: head
+      }));
+    }
+    return _results;
+  };
+
+  tag = function(opts) {
+    var k, t, v, _ref;
+    t = document.createElement((_ref = opts.name) != null ? _ref : 'div');
+    for (k in opts) {
+      if (!__hasProp.call(opts, k)) continue;
+      v = opts[k];
+      switch (k) {
+        case 'name':
+          continue;
+        case 'parent':
+          v.appendChild(t);
+          break;
+        case 'prevSibling':
+          v.parentNode.insertBefore(t, v.nextSibling);
+          break;
+        case 'text':
+          t.appendChild(document.createTextNode(v));
+          break;
+        default:
+          t[k] = v;
+      }
+    }
+    return t;
+  };
+
+  if (document.addEventListener) {
+    document.addEventListener('DOMContentLoaded', go, false);
+  } else if (window.attachEvent) {
+    window.attachEvent('onload', go);
+  }
+
+}).call(this);
